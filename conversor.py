9df5d53840f77c29b7e10433fa29ee1b2c8b21a4
claramohammad <clara.mohammad@sou.inteli.edu.br>
2024-02-09 12:04:25 -0300

    Adiciona comentários

--- conversor.py
+++ conversor.py
@@ -1,11 +1,17 @@
+#Pede a temperatura e a unidade de medida 
 temp = float(input("Coloque a temperatura:"))
 unid = input("Unidade (C ou F): ")
 
+#verifica se a unidade é C ou F e faz a conversão
 if unid.upper() == "C":
     f = 9*temp/5 + 32
     print("A temperatura em F é: ", f)
+
+#verifica se a unidade é C ou F e faz a conversão
 elif unid.upper() == "F":
     c = 5*(temp-32)/9
     print("A temperatura em C é: ", c)
+
+#caso a unidade não seja C ou F emite uma mensagem de erro
 else:
     print("Unidade de temperatura inválida!")
