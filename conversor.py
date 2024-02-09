0f04e459924f6cdc661808b88f9da5bf4eac25a8
claramohammad <clara.mohammad@sou.inteli.edu.br>
2024-02-09 13:37:01 -0300

    Modifica mensagem de saída na branch feature-modificacao

--- conversor.py
+++ conversor.py
@@ -14,4 +14,4 @@ elif unid.upper() == "F":
 
 #caso a unidade não seja C ou F emite uma mensagem de erro
 else:
-    print("Unidade de temperatura inválida!")
+    print("Por favor insira uma unidade de temperatura válida (C ou F)")
