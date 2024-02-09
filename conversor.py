342766ff9e34d9b1d244cc75e862caa599c1c011
claramohammad <clara.mohammad@sou.inteli.edu.br>
2024-02-09 11:38:35 -0300

    Adição do conversor de fahrenheit para celsius

--- conversor.py
+++ conversor.py
@@ -4,3 +4,6 @@ unid = input("Unidade (C ou F): ")
 if unid.upper() == "C":
     f = 9*temp/5 + 32
     print("A temperatura em F é: ", f)
+elif unid.upper() == "F":
+    c = 5*(temp-32)/9
+    print("A temperatura em C é: ", c)
